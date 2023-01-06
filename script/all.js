d556a5c27639cc189c07e0a5d22ac70e1853f293
Keon <h21816595@gmail.com>
2023-01-06 16:20:04 +0800

    ajax錯誤

--- script/all.js
+++ script/all.js
@@ -1,9 +1,35 @@
-let city = "";//紀錄選取的縣市名稱
-let town = [];//紀錄選取的鄉鎮地區
+let cityName = "";//紀錄選取的縣市名稱
+let townName = [];//紀錄選取的鄉鎮地區
 let maskData= [];//紀錄匯出的藥局
+$(function(){
+    // 載入藥局資料
+    $.ajax({
+        type: "GET",
+        url: "points.json",
+        dataType: "json",
+        async: false,
+        success: function(data){
+            maskData = data.features;
+        },
+        error: function(){
+            alert("讀取健保局的所有藥局資料失敗!");
+            console.log('Error: ' + textStatus);
+        }
+    });
+    console.log(maskData);
 
-import { citydata } from "./module/citydata";
 
-// $(function(){
+    // 綁定縣市選單
+    console.log(cityData);
+    $('#TaiwanCity').empty();
+    $('#TaiwanCity').append('<option selected>--請選擇縣市--</option>');
+    
+    // cityName.forEach(e => {
+    //     console.log("e.cityName");
 
-// })
+    // });
+
+
+
+
+});
